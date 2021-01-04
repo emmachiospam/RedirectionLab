77dc1b458bbde394ba7e92fdd647fe4018e1d637
emmachio <emmany2409@gmail.com>
2021-01-04 13:45:54 -0500

    done

---
+++ MakeStars.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class MakeStars{
+
+  public static void main( String[]args ) {
+    String line;
+    Scanner n = new Scanner( System.in );
+    while(n.hasNext()) {
+      String starword = "";
+      line = n.nextLine();
+      Scanner p = new Scanner(line);
+      while(p.hasNext()) {
+        String word = p.next();
+          for(int i = 0; i < word.length(); i++) {
+            starword = starword + "*";
+          }
+        starword = starword + " ";
+      }
+      System.out.println(starword);
+    }
+  }
+}
