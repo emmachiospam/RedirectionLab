ad558a0777ede7f9cf8b2ddff708500613597a91
emmachio <echio20@stuy.edu>
2021-01-05 19:31:37 -0500

    done with PigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -6,7 +6,7 @@ public class PigLatin{
     Scanner n = new Scanner( System.in );
     while(n.hasNext()) {
       line = n.nextLine();
-      System.out.println(pigLatinSimple(line));
+      System.out.println(pigLatin(line));
     }
   }
 
@@ -27,6 +27,43 @@ public class PigLatin{
           pigword = pigword + word.substring(i, i+1);
         }
         pigword = pigword + word.substring(0,1) + "ay ";
+      }
+      line = line + pigword;
+    }
+    return line;
+  }
+
+  public static String pigLatin(String s) {
+    String[] duo = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl",
+              "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    String line = "";
+    Scanner p = new Scanner(s);
+    while(p.hasNext()) {
+      String word = p.next();
+      String pigword = "";
+      for(int i = 0; i < duo.length; i++) {
+        String beg = duo[i];
+        if(word.length() >= 2 && (word.substring(0,2)).equals(beg)) {
+          for(int j = 2; j < word.length(); j++) {
+              pigword = pigword + word.substring(j, j+1);
+          }
+          pigword = pigword + word.substring(0,2) + "ay ";
+          i = duo.length;
+        }
+        else if((word.charAt(0) == 'a' || word.charAt(0) == 'e' || word.charAt(0) == 'i' || word.charAt(0) == 'o' || word.charAt(0) == 'u') && i == duo.length - 1) {
+          for(int j = 0; j < word.length(); j++) {
+              pigword = pigword + word.substring(j, j+1);
+          }
+          pigword = pigword + "hay ";
+        }
+        else{
+          if(i == duo.length - 1) {
+            for(int j = 1; j < word.length(); j++) {
+                pigword = pigword + word.substring(j, j+1);
+            }
+            pigword = pigword + word.substring(0,1) + "ay ";
+          }
+        }
       }
       line = line + pigword;
     }
