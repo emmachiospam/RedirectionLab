fa0bb1d8ca4396d03f2a060654710180c00a7186
emmachio <echio20@stuy.edu>
2021-01-05 19:51:13 -0500

    done

--- PigLatin.java
+++ PigLatin.java
@@ -6,7 +6,7 @@ public class PigLatin{
     Scanner n = new Scanner( System.in );
     while(n.hasNext()) {
       line = n.nextLine();
-      System.out.println(pigLatin(line));
+      System.out.println(pigLatinBest(line));
     }
   }
 
@@ -16,6 +16,7 @@ public class PigLatin{
     while(p.hasNext()) {
       String pigword = "";
       String word = p.next();
+      word = word.toLowerCase();
       if(word.charAt(0) == 'a' || word.charAt(0) == 'e' || word.charAt(0) == 'i' || word.charAt(0) == 'o' || word.charAt(0) == 'u') {
         for(int i = 0; i < word.length(); i++) {
           pigword = pigword + word.substring(i, i+1);
@@ -40,6 +41,7 @@ public class PigLatin{
     Scanner p = new Scanner(s);
     while(p.hasNext()) {
       String word = p.next();
+      word = word.toLowerCase();
       String pigword = "";
       for(int i = 0; i < duo.length; i++) {
         String beg = duo[i];
@@ -64,6 +66,54 @@ public class PigLatin{
             pigword = pigword + word.substring(0,1) + "ay ";
           }
         }
+      }
+      line = line + pigword;
+    }
+    return line;
+  }
+
+  public static String pigLatinBest(String s) {
+    String[] duo = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl",
+              "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    String line = "";
+    Scanner p = new Scanner(s);
+    while(p.hasNext()) {
+      String word = p.next();
+      word = word.toLowerCase();
+      String endPun = "";
+      String pigword = "";
+      if(Character.isLetter((word.charAt(0)))) {
+        if(Character.isLetter((word.charAt(word.length()-1))) == false) {
+          endPun = word.substring(word.length() - 1);
+          word = word.substring(0, word.length()-1);
+        }
+        for(int i = 0; i < duo.length; i++) {
+          String beg = duo[i];
+          if(word.length() >= 2 && (word.substring(0,2)).equals(beg)) {
+            for(int j = 2; j < word.length(); j++) {
+                pigword = pigword + word.substring(j, j+1);
+            }
+            pigword = pigword + word.substring(0,2) + "ay" + endPun + " ";
+            i = duo.length;
+          }
+          else if((word.charAt(0) == 'a' || word.charAt(0) == 'e' || word.charAt(0) == 'i' || word.charAt(0) == 'o' || word.charAt(0) == 'u') && i == duo.length - 1) {
+            for(int j = 0; j < word.length(); j++) {
+                pigword = pigword + word.substring(j, j+1);
+            }
+            pigword = pigword + "hay" + endPun + " ";
+          }
+          else{
+            if(i == duo.length - 1) {
+              for(int j = 1; j < word.length(); j++) {
+                  pigword = pigword + word.substring(j, j+1);
+              }
+              pigword = pigword + word.substring(0,1) + "ay" + endPun + " ";
+            }
+          }
+        }
+      }
+      else {
+        pigword = word + " ";
       }
       line = line + pigword;
     }
